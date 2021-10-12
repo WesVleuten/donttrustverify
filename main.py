ccfbee29ae7a268a90bfbd25c13fe4b1299af21f
Wes van der Vleuten <16665772+WesVleuten@users.noreply.github.com>
2021-10-13 01:29:07 +0200

    Updated bitcoin core references

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 # Sat per coin
-# https://github.com/bitcoin/bitcoin/blob/3c776fdcec176ffaa2056633fa2b4e737cda29ce/src/consensus/amount.h#L25
+# https://github.com/bitcoin/bitcoin/blob/6f0cbc75be7644c276650fd98bfdb6358b827399/src/consensus/amount.h#L14
 COIN = 100000000
 
 # Main chain subsidy halving interval
-# https://github.com/bitcoin/bitcoin/blob/3c776fdcec176ffaa2056633fa2b4e737cda29ce/src/chainparams.cpp#L67
+# https://github.com/bitcoin/bitcoin/blob/6f0cbc75be7644c276650fd98bfdb6358b827399/src/chainparams.cpp#L67
 nSubsidyHalvingInterval = 210000
 
 # Block subsidy algorithm
-# https://github.com/bitcoin/bitcoin/blob/master/src/validation.cpp#L1068
+# https://github.com/bitcoin/bitcoin/blob/6f0cbc75be7644c276650fd98bfdb6358b827399/src/validation.cpp#L1068
 def getblocksubsidy(nHeight):
     halvings = (int)(nHeight / nSubsidyHalvingInterval)
 
